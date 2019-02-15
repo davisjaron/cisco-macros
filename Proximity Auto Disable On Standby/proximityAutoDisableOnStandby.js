783f45cf2d43003869d98f18c43bb3fbf661fd7b
davisjaron <47332585+davisjaron@users.noreply.github.com>
2019-02-15 07:41:22 -0700

    Add files via upload

--- Proximity Auto Disable On Standby/proximityAutoDisableOnStandby.js
+++ Proximity Auto Disable On Standby/proximityAutoDisableOnStandby.js
@@ -7,7 +7,7 @@ Purpose:
 This macro automatically disables Cisco Proximity whenever the system enters standby.
 
 Important Notes:
-- This was written specifically for the 6901 S. Havana St. Cafe, to prevent the entire AV system from turning on when users with Proximity walk into the cafe.
+- This was written specifically for the Havana Cafe, to prevent the entire AV system from turning on when users with Proximity walk into the cafe.
 - There is no XML file required for this macro, simply upload this file as a macro within the codec.
 */
 
