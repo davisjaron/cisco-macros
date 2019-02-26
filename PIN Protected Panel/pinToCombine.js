537a53e9fb5e73a12ca7a643c224410931f0bb0c
davisjaron <47332585+davisjaron@users.noreply.github.com>
2019-02-26 10:09:53 -0700

    Add files via upload
    I was looking for a way to password protect a panel, so that a user must know the password to access the settings.  I couldn't find anything, so I decided to write one up.

---
+++ PIN Protected Panel/pinToCombine.js
@@ -0,0 +1,32 @@
+const xapi = require('xapi');
+
+var pinEntered = '';
+const pin = '12345';
+
+xapi.event.on('UserInterface Extensions Panel Clicked', (event) => {
+    if(event.PanelId == 'Divisible_Room_Control_PanelID'){
+      showPinPad('Enter Divisible Room Control PIN');
+    }
+});
+
+function showPinPad(text){
+  xapi.command('UserInterface Message TextInput Display', {
+    Title: 'Enter PIN',
+    Text: 'A PIN is required to put these rooms into combined mode.',
+    InputType: 'PIN',
+    KeyboardState: 'Open',
+    Duration: '0' 
+  })
+  .then(checkPinMatch);
+}
+	
+function checkPinMatch(event) {
+  xapi.event.on('UserInterface Message TextInput Response', (event) => {
+    if(event.Text === pin){
+      xapi.command('UserInterface Message TextInput Clear');
+    }
+    if(event.Text !== pin) {
+      xapi.command('UserInterface Extensions Panel Close');
+    }
+  });
+}
